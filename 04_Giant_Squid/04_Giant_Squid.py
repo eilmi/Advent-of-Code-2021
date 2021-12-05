613599a9e2803af11b4e4fd3deb837e39a7b63dc
Philipp Eilmsteiner <philipp@eilm.at>
2021-12-05 09:05:20 +0100

    sped up Giant Squid by a lot

--- 04_Giant_Squid/04_Giant_Squid.py
+++ 04_Giant_Squid/04_Giant_Squid.py
@@ -10,7 +10,7 @@ bingo_numbers = f[0].strip().split(",")# extract the numbers from the first line
 for i in range(0, len(bingo_numbers)): #convert the string array into an int array
     bingo_numbers[i] = int(bingo_numbers[i])
 
-#find all empty lines  and therefor the start and endings of all present boards
+#find all empty lines  and therefor the start and endings of all given boards
 boardends=[]
 for i in range(1,len(f)): # iterate over all lines of the file excluding the first one
     if f[i]=="\n": #check if line of file is empty
@@ -33,39 +33,41 @@ for i in range(len(boardends)-1):
 nparray = np.array(boards,dtype=int)
 
 
-def checkifwon(numbers,boardarrays):
-    won_boards=[] 
+def checkifwon(numbers,boardarrays,wonboards=[]):
+    _won_boards=wonboards
     for i in range(boardarrays.shape[0]): #iterate over all boards
-
+        if i in _won_boards: # if board with i as index has already won skip checking and move on to next one
+            continue
         for x in range(boardarrays.shape[1]): #check if a row of a board has won
             row=boardarrays[i,x,:] # get row of board
             if all(item in numbers for item in row):
-                won_boards.append(i) #add index of this board to the won_board array
+                _won_boards.append(i) #add index of this board to the won_board array
         
         for x in range(boardarrays.shape[2]): #check if a column of a board has won
             column=boardarrays[i,:,x] # get column of board
             if all(item in numbers for item in column): # check if all numbers of this column are in the list of already drawn numbers
-                won_boards.append(i) # add index of this board to the won_board array
+                _won_boards.append(i) # add index of this board to the won_board array
 
-    return won_boards
+    return list(dict.fromkeys(_won_boards)) # remove duplicates from array and return it 
 
 
 a_list=list(range(0,len(boards)))
 firstwinner=True
+result=[]
 
 for a in range(0,len(bingo_numbers)):
-    nums=bingo_numbers[0:a+1]
+    nums=bingo_numbers[0:a+1] # array of already drawn numbers
     #print(nums)
-    result = checkifwon(nums,nparray)
-    yet_to_win_boards = [item for item in a_list if item not in list(dict.fromkeys(result))]
+    result = checkifwon(nums,nparray,result) # check which boards win with these drawn numbers
+    yet_to_win_boards = [item for item in a_list if item not in result]
     #print(bingo_numbers[a],yet_to_win_boards)
 
 
     if (len(result)==1 and firstwinner==True):
         firstwinner=False
         winnernumber=a
-        winnerindex=result[0]
-        print("Board",winnerindex,"just won when",bingo_numbers[a],"was called")
+        winnerboard=result[0]
+        print("Board",winnerboard,"just won when",bingo_numbers[a],"was called")
 
     if (len(yet_to_win_boards)==1):
         loserindex=yet_to_win_boards[0]
@@ -75,7 +77,7 @@ for a in range(0,len(bingo_numbers)):
         losernumber=a
         break
 
-winningboard=np.array(nparray[winnerindex])
+winningboard=np.array(nparray[winnerboard])
 losingboard=np.array(nparray[loserindex])
 
 #print(winningboard)
